26c0ad103d31fc9386eb6b12977a490d4c75d000
valerazz8 <greerz1212@gmail.com>
2021-05-18 15:15:28 +0300

    dist

---
+++ js/chunk-2d21b386.ee29edff.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21b386"],{bf9e:function(n,c,o){"use strict";o.r(c);const s={};c["default"]=s}}]);
+//# sourceMappingURL=chunk-2d21b386.ee29edff.js.map
